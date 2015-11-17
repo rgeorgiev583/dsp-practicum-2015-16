904594e54f2a0ff53278e73cda7ab024bd279410
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-11-17 15:54:13 +0200

    added assignment ID as command-line argument: a new directory is now created in `solutions' with the assignment ID as name and the solution source files are moved there before merging

--- util/prmerge.sh
+++ util/prmerge.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 [ -z $1 ] && exit 1
+[ -z $2 ] && exit 1
 
 git checkout -b $1-master master
 git pull https://github.com/$1/dsp-practicum-2015-16.git master
+
+cd ..
+mkdir -p 8/solutions/$2
+fn=$(git diff --name-only master HEAD | head -1 | xargs dirname)
+git mv $fn 8/solutions/$2
+git commit -m "pre-automerge: moved source files for FN $fn to the assignment $2 directory in \`solutions'"
+
 git checkout master
 git merge --no-ff $1-master
 git push origin master
