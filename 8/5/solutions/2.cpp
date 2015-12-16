d41bc8b65b2e3a365d8c5ecd0ab4fb68949d582d
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-12-16 02:08:17 +0200

    rename: refactoring names: they are almost readable now

--- 8/5/solutions/2.cpp
+++ 8/5/solutions/2.cpp
@@ -3,69 +3,74 @@
 #include <cstdlib>
 #include <cstring>
 
-#define MAX_SIZE 1000
+#define BUFFER_SIZE 1000
 
 using namespace std;
 
-struct rep_expr_t
+struct RepetitiveStringExpression
 {
-    size_t count, expr_pos, str_pos;
+    size_t count, expressionPosition, stringPosition;
 };
+
+static inline bool isDigit(char c)
+{
+    return c >= '0' && c <= '9';
+}
 
 int main()
 {
-    char* str = new char[MAX_SIZE];
-    cin.getline(str, MAX_SIZE);
-    stack<rep_expr_t> state;
-    rep_expr_t expr;
-    char* s = str;
+    char* str = new char[BUFFER_SIZE];
+    cin.getline(str, BUFFER_SIZE);
+    stack<RepetitiveStringExpression> parserState;
+    RepetitiveStringExpression expr;
+    char* iter = str;
 
-    while (*s)
-        if (*s >= '0' && *s <= '9')
+    while (*iter)
+        if (isDigit(*iter))
         {
-            expr.count = atoi(s);
-            char* expr_begin = s;
+            expr.count = atoi(iter);
+            char* expressionBeginning = iter;
 
-            while (*s && *s >= '0' && *s <= '9')
-                s++;
+            while (*iter && *iter >= '0' && *iter <= '9')
+                iter++;
 
-            if (*s++ == '(')
+            if (*iter++ == '(')
             {
-                expr.expr_pos = (size_t)(expr_begin - str);
-                expr.str_pos  = (size_t)(s - str);
-                state.push(expr);
+                expr.expressionPosition = (size_t)(expressionBeginning - str);
+                expr.stringPosition  = (size_t)(iter - str);
+                parserState.push(expr);
             }
         }
-        else if (*s == ')')
+        else if (*iter == ')')
         {
-            expr = state.top();
-            state.pop();
-            size_t rep_str_len = (size_t) (s - str - expr.str_pos), rep_len = expr.count * rep_str_len;
+            expr = parserState.top();
+            parserState.pop();
+            size_t rep_str_len = (size_t) (iter - str - expr.stringPosition), rep_len = expr.count * rep_str_len;
             char* rep_str = new char[rep_len + 1];
-            strncpy(rep_str, str + expr.str_pos, rep_str_len);
+            strncpy(rep_str, str + expr.stringPosition, rep_str_len);
             rep_str[rep_str_len] = '\0';
 
             for (size_t i = 2; i <= expr.count; i++)
             {
-                strncat(rep_str, str + expr.str_pos, rep_str_len);
+                strncat(rep_str, str + expr.stringPosition, rep_str_len);
                 rep_str[i * rep_str_len] = '\0';
             }
 
             rep_str[rep_len] = '\0';
-            size_t rest_len = strlen(++s);
-            char* new_str = new char[expr.expr_pos + rep_len + rest_len + 1];
-            strncpy(new_str, str, expr.expr_pos);
-            new_str[expr.expr_pos] = '\0';
+            size_t rest_len = strlen(++iter);
+            char* new_str = new char[expr.expressionPosition + rep_len + rest_len + 1];
+            strncpy(new_str, str, expr.expressionPosition);
+            new_str[expr.expressionPosition] = '\0';
             strcat(new_str, rep_str);
-            strcat(new_str, s);
-            rep_str[expr.expr_pos + rep_len + rest_len] = '\0';
-            s = new_str + expr.expr_pos + rep_len;
+            strcat(new_str, iter);
+            rep_str[expr.expressionPosition + rep_len + rest_len] = '\0';
+            iter = new_str + expr.expressionPosition + rep_len;
             delete[] rep_str;
             delete[] str;
             str = new_str;
         }
         else
-            s++;
+            iter++;
 
     cout << str << endl;
     return 0;
