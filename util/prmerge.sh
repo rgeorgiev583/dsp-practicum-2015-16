3c536b60f086b24ff14dc68bf83d773c3651700c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-01-11 14:01:32 +0200

    fix: split automerge script into two steps

--- util/prmerge.sh
+++ util/prmerge.sh
@@ -2,16 +2,24 @@
 
 [ -z $1 ] && exit 1
 [ -z $2 ] && exit 1
+action=$1
+name=$2
 
-git checkout -b $1-master master
-git pull https://github.com/$1/dsp-practicum-2015-16.git master
+if [ $action = begin ]; then
+    git checkout -b $name-master master
+    git pull https://github.com/$name/dsp-practicum-2015-16.git master
 
-cd ..
-mkdir -p 8/solutions/$2
-fn=$(git diff --name-only master HEAD | head -1 | xargs dirname)
-git mv $fn 8/solutions/$2
-git commit -m "pre-automerge: moved source files for FN $fn to the assignment $2 directory in \`solutions'"
+    olddir=$(git diff --name-only master HEAD | head -1 | xargs dirname)
+    [ -n $3 ] && fn=$3 || fn=$olddir
+    newdir=8/solutions/$fn
+    mkdir -p $newdir
+    git mv $olddir $newdir
 
-git checkout master
-git merge --no-ff $1-master
-git push origin master
+    git commit -m "automerge started: moved source files for FN \`$olddir' to the \`$newdir' directory in \`solutions'"
+elif [ $action = finish ]; then
+    git commit -m "automerge completed: assignment has been graded"
+
+    git checkout master
+    git merge --no-ff $name-master
+    git push origin master
+fi
