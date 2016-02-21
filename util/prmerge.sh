7d5e23105ab80a0f2d3adad16cf45cd40c36989c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-02-22 00:22:39 +0200

    fixed automerge script

--- util/prmerge.sh
+++ util/prmerge.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
+[ -z $ASSIGN_DIR ] && ASSIGN_DIR=8/solutions
 [ -z $1 ] && exit 1
 [ -z $2 ] && exit 1
 action=$1
 name=$2
 
-if [ $action = begin ]; then
+if [ $action = init ]; then
     git checkout -b $name-master master
     git pull https://github.com/$name/dsp-practicum-2015-16.git master
 
     olddir=$(git diff --name-only master HEAD | head -1 | xargs dirname)
     [ -n $3 ] && fn=$3 || fn=$olddir
-    newdir=8/solutions/$fn
+    newdir=$ASSIGN_DIR/$fn
     mkdir -p $newdir
     git mv $olddir $newdir
 
-    git commit -m "automerge started: moved source files for FN \`$olddir' to the \`$newdir' directory in \`solutions'"
-elif [ $action = finish ]; then
-    git commit -m "automerge completed: assignment has been graded"
+    git commit -m "automerge started: moved source files for FN $fn to the $newdir directory"
+elif [ $action = commit ]; then
+    git commit -m "automerge completed: assignment submission by FN $fn has been graded"
 
     git checkout master
     git merge --no-ff $name-master
